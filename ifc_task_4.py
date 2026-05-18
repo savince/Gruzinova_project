f647bd976e21ab987ae21bb4d9d23553a3978912
Gruzinova Alexia <gruzinova.alexia@gmail.com>
2026-05-18 19:24:39 +0300

    выполнено 5. анализ размеров дверей

--- ifc_task_4.py
+++ ifc_task_4.py
@@ -4,7 +4,7 @@ filepath = "Example_1.ifc"
 
 model = ifcopenshell.open(filepath)
 
-# Получаем используемую схему IFC (например, IFC2X3, IFC4)
+# Получаем используемую схему IFC
 print(f"Схема IFC: {model.schema}")
 
 # Получаем все объекты типа IfcBuildingStorey (этажи)
@@ -17,6 +17,3 @@ print(f"Количество этажей: {len(storeys)}")
 for storey in storeys:
     elevation = getattr(storey, "Elevation", None)
     print(f"Этаж: {storey.Name}, Elevation={elevation}")
-
-# Краткая строка-заголовок для подписи окна/результата
-print("TITLE: Информация об этажах модели")
